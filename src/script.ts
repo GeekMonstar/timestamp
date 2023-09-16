82e062b3e8ed38cf6584bf26f44787ffa31d53c3
Juan Vianney <juanvianneynm@gmail.com>
2023-09-16 15:51:51 +0200

    Update-1

--- src/script.ts
+++ src/script.ts
@@ -2,10 +2,23 @@ const daysLayout = document.querySelector(".days")!;
 const hoursLayout = document.querySelector(".hours")!;
 const minutesLayout = document.querySelector(".minutes")!;
 const secondsLayout = document.querySelector(".seconds")!;
+const dateLayout = document.querySelector(".date");
 const stampInTheDay = 86400;
 let angle = 36;
-let date = new Date("2024-08-14");
+const year = new Date().getFullYear();
+console.log(new Date().getFullYear());
+let date = null;
+let dateString = "";
+if (new Date(`${year}-08-14`) > new Date()) {
+  date = new Date(`${year}-08-14`);
+  dateString = `14/08/${year}`;
+} else {
+  date = new Date(`${year + 1}-08-14`);
+  dateString = `14/08/${year + 1}`;
+}
 let targetTimestamp = date.getTime();
+dateLayout.textContent = dateString;
+console.log(date);
 
 update();
 setInterval(() => {
@@ -23,12 +36,12 @@ function update() {
   let hours = Math.trunc((duration % stampInTheDay) / 3600);
   let minutes = Math.floor((duration / 60) % 60);
   let seconds = Math.trunc((duration % stampInTheDay) % 60);
-  daysLayout.textContent = `${daysLeft}jour${daysLeft > 1 ? "s" : ""}`;
-  hoursLayout.textContent = `${hours}heure${hours > 1 ? "s" : ""}`;
+  daysLayout.textContent = `${daysLeft} jour${daysLeft > 1 ? "s" : ""}`;
+  hoursLayout.textContent = `${hours} heure${hours > 1 ? "s" : ""}`;
   minutesLayout.textContent = `${
     minutes >= 10 ? minutes : `0${minutes}`
-  }minute${minutes > 1 ? "s" : ""}`;
+  } minute${minutes > 1 ? "s" : ""}`;
   secondsLayout.textContent = `${
     seconds >= 10 ? seconds : `0${seconds}`
-  }seconde${seconds > 1 ? "s" : ""}`;
+  } seconde${seconds > 1 ? "s" : ""}`;
 }
